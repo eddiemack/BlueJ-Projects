7c6a9adede721b3570801fefb0850516113b66c6
Greg Krsak <greg.krsak@gmail.com>
2013-12-18 09:13:51 -0800

    drive() is now a more-accurate simulation
    I expanded the drive() method of the Car class to model a couple of
    things:
    - Maximum distance possible
    - Distance the driver intends to drive
    - Actual distance driven
    
    Additionally, I added range checking: maximumDistance and
    intendedDistance can’t be less than zero; actualDistance can’t be
    greater than maximumDistance.
    
    Finally, drive() now returns the actualDistance (actual distance
    driven), as a double.

--- Car Project/Car.java
+++ Car Project/Car.java
@@ -31,11 +31,36 @@ public class Car
     
     /**
      * Drives the car however many miles the user wants, as you drive, it reduces the amount of gas in the gas tank
-     * @param miles # of miles the car will drive
+     * @param miles # of miles the car will attempt to drive
+     * @return number of miles actually driven
      */
-    public void drive (double miles)
+    public double drive (double miles)
     {
-        gasLevel = gasLevel - (miles / mpg);
+        double result;
+        
+        double maximumDistance;
+        double intendedDistance;
+        double actualDistance;
+        
+        maximumDistance = getGasLevel() * mpg;
+        if (maximumDistance < 0) {
+            maximumDistance = 0;
+        }
+        
+        intendedDistance = miles;
+        if (intendedDistance < 0) {
+            intendedDistance = 0;
+        }
+        
+        actualDistance = intendedDistance;
+        if (intendedDistance > maximumDistance) {
+            actualDistance = maximumDistance;
+        }
+        
+        gasLevel -= (actualDistance / mpg);
+        result = actualDistance;
+        
+        return result;
     }
     
     /**
